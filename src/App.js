731d4eb31c5c9d877428b5eccb6d8205d90bd7fd
harry-pm <harrypenderedmazer@gmail.com>
2020-02-26 16:08:27 +0000

    fetch api results and set them to state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+
+  state = {
+    results: [],
+  };
+
+  getResults = () => {
+    axios.get('https://demo.staging.energyhelpline-aws.com/api/results/a8823b4b-1abe-41de-a5b3-ab6700c08d98').then(res => {
+      this.setState({ results: res.data});
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getResults();
+  }
   
   render () {
     return (
       <div className="App">
-        <p>
-          hello
-        </p>     
+
+        <div className="form">
+          <form onSubmit={this.handleSubmit}>
+            <input type="submit" value="display results"></input>
+          </form>   
+        </div>
+
+        <div className="results">
+          {/* <p>{this.state.results.gasResults}</p>
+          <p>{this.state.results.elecResults}</p> */}
+        </div>
+
       </div>
     );
   }
